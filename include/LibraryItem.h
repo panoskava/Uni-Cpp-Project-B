d55f43ec28baf5c7f1f2b10d4a29c00155d027ba
panoskava <panoskava2006@gmail.com>
2025-05-23 21:15:09 +0300

    Created 1st class (abstract)

--- include/LibraryItem.h
+++ include/LibraryItem.h
@@ -5,4 +5,19 @@
 #include <string>
 #include <vector>
 
+class LibraryItem
+{
+protected:
+    std::string code;
+    std::string title;
+
+public:
+    // Constructor declaration
+    LibraryItem(const std::string &code, const std::string &title);
+
+    // Method declarations
+    void show() const;
+    virtual void printInfo() const = 0;
+};
+
 #endif
--- src/LibraryItem.cpp
+++ src/LibraryItem.cpp
@@ -2,3 +2,15 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
+// Constructor implementation
+LibraryItem::LibraryItem(const std::string &code, const std::string &title)
+    : code(code), title(title)
+{
+}
+
+// Method implementation
+void LibraryItem::show() const
+{
+    std::cout << "Code: " << code << ", Title: " << title << std::endl;
+}
