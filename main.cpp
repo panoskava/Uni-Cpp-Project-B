0d8eb14692eed4bb36a943433e56395884ffbc43
panoskava <panoskava2006@gmail.com>
2025-05-24 12:09:53 +0300

    Refactor main function to include item printing and deletion

--- main.cpp
+++ main.cpp
@@ -13,4 +13,16 @@ int main()
     // Add LibraryItem objects to the vector
     item.push_back(new Magazine("j001", "Tech Today", 5, 3));
     item.push_back(new Book("B031", "C++ Programming", "Bjarne Stroustrup", 2013));
+
+    // Print information for each item in the vector
+    for (LibraryItem *libItem : item)
+    {
+        libItem->printInfo();
+    }
+
+    // Delete created items
+    for (LibraryItem *libItem : item)
+    {
+        delete libItem;
+    }
 }
