25ae98b7bb9b17b74ba824ba92b79db3b1da5612
panoskava <panoskava2006@gmail.com>
2025-05-24 12:02:32 +0300

    Added vector and default items

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <vector>
 
+// Include headers for each class
+#include "include/LibraryItem.h"
+#include "include/Magazine.h"
+#include "include/Book.h"
+
 int main()
 {
+    std::vector<LibraryItem *> item;
+
+    // Add LibraryItem objects to the vector
+    item.push_back(new Magazine("j001", "Tech Today", 5, 3));
+    item.push_back(new Book("B031", "C++ Programming", "Bjarne Stroustrup", 2013));
 }
